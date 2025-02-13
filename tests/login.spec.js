0b7b8d9827d917abbc2c365747f11441334affec
Bruno <brunodorea@outlook.com.br>
2025-02-13 13:08:40 -0300

    :test_tube: test: add teste de login valido + refatoraçãono código

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,23 +1,41 @@
-import { test, expect } from '@playwright/test';
-import { cp } from 'fs';
+import { test, expect } from '@playwright/test'
+import { obterCodigo2FA } from '../support/db'
+import { LoginPage } from '../pages/LoginPage'
+import { DashPage } from '../pages/DashPage'
 
 test('Login com código de autenticação inválido', async ({ page }) => {
-  const usuario = {
-    cpf: '00000014141',
-    senha: '147258',
-  }
+    const loginPage = new LoginPage(page)
 
-  await page.goto('http://paybank-mf-auth:3000/');
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
+    const usuario = {
+        cpf: '00000014141',
+        senha: '147258',
+    }
 
-  for (const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
+    await loginPage.acessaPagina()
+    await loginPage.informaCpf(usuario.cpf)
+    await loginPage.informaSenha(usuario.senha)
+    await loginPage.informa2FA('123456')
 
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+    await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.')
+})
 
-  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
-});
+test('Login com código de autenticação válido', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const dashPage = new DashPage(page)
+
+    const usuario = {
+        cpf: '00000014141',
+        senha: '147258',
+    }
+
+    await loginPage.acessaPagina()
+    await loginPage.informaCpf(usuario.cpf)
+    await loginPage.informaSenha(usuario.senha)
+
+    await page.waitForTimeout(3000) // temporário
+    const codigo = await obterCodigo2FA()
+    await loginPage.informa2FA(codigo)
+    await page.waitForTimeout(3000)
+
+    expect(await dashPage.obterSaldo()).toContainText('R$ 5.000,00')
+})
