6af55a6cded4cab3ce5d09e7c131cf41cd12abb1
Bruno <brunodorea@outlook.com.br>
2025-02-13 21:19:19 -0300

    :test_tube: test: teste de login valido consumindo redis + refatoração

--- playwright.config.js
+++ playwright.config.js
@@ -38,10 +38,10 @@ export default defineConfig({
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //     name: 'firefox',
+        //     use: { ...devices['Desktop Firefox'] },
+        // },
         // {
         //     name: 'webkit',
         //     use: { ...devices['Desktop Safari'] },
@@ -56,14 +56,14 @@ export default defineConfig({
         //   use: { ...devices['iPhone 12'] },
         // },
         /* Test against branded browsers. */
-        {
-            name: 'Microsoft Edge',
-            use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        },
-        {
-            name: 'Google Chrome',
-            use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        },
+        // {
+        //     name: 'Microsoft Edge',
+        //     use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //     name: 'Google Chrome',
+        //     use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
     ],
 
     /* Run your local dev server before starting the tests */
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test'
 import { obterCodigo2FA } from '../support/db'
+
 import { LoginPage } from '../pages/LoginPage'
 import { DashPage } from '../pages/DashPage'
+
+import { cleanJobs, getJob } from '../support/redis'
 
 test('Login com código de autenticação inválido', async ({ page }) => {
     const loginPage = new LoginPage(page)
@@ -19,7 +22,7 @@ test('Login com código de autenticação inválido', async ({ page }) => {
     await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.')
 })
 
-test('Login com código de autenticação válido', async ({ page }) => {
+test('Login com código de autenticação válido via DB', async ({ page }) => {
     const loginPage = new LoginPage(page)
     const dashPage = new DashPage(page)
 
@@ -28,14 +31,39 @@ test('Login com código de autenticação válido', async ({ page }) => {
         senha: '147258',
     }
 
+    await cleanJobs()
+
     await loginPage.acessaPagina()
     await loginPage.informaCpf(usuario.cpf)
     await loginPage.informaSenha(usuario.senha)
 
-    await page.waitForTimeout(3000) // temporário
-    const codigo = await obterCodigo2FA()
-    await loginPage.informa2FA(codigo)
-    await page.waitForTimeout(3000)
+    await page.getByRole('heading', { name: 'Verificação em duas etapas' }).waitFor({ timeout: 3000 })
 
-    expect(await dashPage.obterSaldo()).toContainText('R$ 5.000,00')
+    const code = await obterCodigo2FA(usuario.cpf)
+    await loginPage.informa2FA(code)
+
+    await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+})
+
+test('Login com código de autenticação válido via Redis', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const dashPage = new DashPage(page)
+
+    const usuario = {
+        cpf: '00000014141',
+        senha: '147258',
+    }
+
+    await cleanJobs()
+
+    await loginPage.acessaPagina()
+    await loginPage.informaCpf(usuario.cpf)
+    await loginPage.informaSenha(usuario.senha)
+
+    await page.getByRole('heading', { name: 'Verificação em duas etapas' }).waitFor({ timeout: 3000 })
+
+    const code = await getJob()
+    await loginPage.informa2FA(code)
+
+    await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 })
